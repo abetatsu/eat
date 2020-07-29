053af6dc72ef7d85661fa8a965dd3b8bae5097ff
Tatsu <tatsuhiroabe0127@gmail.com>
2020-07-30 00:07:49 +0900

    成人女性パターンの追加(登録フォーム、栄養素)

--- app/Http/Controllers/BeefController.php
+++ app/Http/Controllers/BeefController.php
@@ -70,18 +70,34 @@ class BeefController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function show(Beef $beef)
-    {
+    {   
         $user = Auth::user();
-        $dayNeedsCalories = 2500 - $user->totalCalories;
-        $dayNeedsProtein = 100 - $user->totalProtein;
-        $dayNeedsFat = 80 - $user->totalFat;
-        $dayNeedsCarb = 350 - $user->totalCarb;
-        $dayNeedsSodium = 8 - $user->totalSodium;
-        $weekNeedsCalories = 17500 - $user->totalWeekCalories;
-        $weekNeedsProtein = 700 - $user->totalWeekProtein;
-        $weekNeedsFat = 560 - $user->totalWeekFat;
-        $weekNeedsCarb = 2450 - $user->totalWeekCarb;
-        $weekNeedsSodium = 56 - $user->totalWeekSodium;
+
+        if($user->gender === 'male')
+        {
+            $dayNeedsCalories = 2500 - $user->totalCalories;
+            $dayNeedsProtein = 100 - $user->totalProtein;
+            $dayNeedsFat = 80 - $user->totalFat;
+            $dayNeedsCarb = 350 - $user->totalCarb;
+            $dayNeedsSodium = 8 - $user->totalSodium;
+            $weekNeedsCalories = 17500 - $user->totalWeekCalories;
+            $weekNeedsProtein = 700 - $user->totalWeekProtein;
+            $weekNeedsFat = 560 - $user->totalWeekFat;
+            $weekNeedsCarb = 2450 - $user->totalWeekCarb;
+            $weekNeedsSodium = 56 - $user->totalWeekSodium;
+
+        } elseif($user->gender === 'female') {
+            $dayNeedsCalories = 1800 - $user->totalCalories;
+            $dayNeedsProtein = 70 - $user->totalProtein;
+            $dayNeedsFat = 50 - $user->totalFat;
+            $dayNeedsCarb = 290 - $user->totalCarb;
+            $dayNeedsSodium = 7 - $user->totalSodium;
+            $weekNeedsCalories = 12600 - $user->totalWeekCalories;
+            $weekNeedsProtein = 490 - $user->totalWeekProtein;
+            $weekNeedsFat = 350 - $user->totalWeekFat;
+            $weekNeedsCarb = 2030 - $user->totalWeekCarb;
+            $weekNeedsSodium = 49 - $user->totalWeekSodium;
+        }
 
         return view('beef.show', 
         [
