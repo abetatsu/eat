40159dadfa3dcaa05fd1456961bfc40285ceb1b1
Tatsu <tatsuhiroabe0127@gmail.com>
2020-07-30 11:45:14 +0900

    商品詳細ページでのエラーの修正

--- routes/web.php
+++ routes/web.php
@@ -12,25 +12,25 @@
 */
 
 Route::get('/beef', 'BeefController@index');
-Route::get('/beef/{beef}', 'BeefController@show');
+Route::get('/beef/{beef}', 'BeefController@show')->middleware('auth');
 
 Route::get('/curry', 'CurryController@index');
-Route::get('/curry/{curry}', 'CurryController@show');
+Route::get('/curry/{curry}', 'CurryController@show')->middleware('auth');
 
 Route::get('/morning', 'MorningController@index');
-Route::get('/morning/{morning}', 'MorningController@show');
+Route::get('/morning/{morning}', 'MorningController@show')->middleware('auth');
 
 Route::get('/other', 'OtherController@index');
-Route::get('/other/{other}', 'OtherController@show');
+Route::get('/other/{other}', 'OtherController@show')->middleware('auth');
 
 Route::get('/setmeal', 'SetmealController@index');
-Route::get('/setmeal/{setmeal}', 'SetmealController@show');
+Route::get('/setmeal/{setmeal}', 'SetmealController@show')->middleware('auth');
 
 Route::get('/sidemenu', 'SidemenuController@index');
-Route::get('/sidemenu/{sidemenu}', 'SidemenuController@show');
+Route::get('/sidemenu/{sidemenu}', 'SidemenuController@show')->middleware('auth');
 
 Route::post('/user', 'UserController@update');
-Route::get('/user/{user}', 'UserController@show');
+Route::get('/user/{user}', 'UserController@show')->middleware('auth');
 
 
 Auth::routes();
